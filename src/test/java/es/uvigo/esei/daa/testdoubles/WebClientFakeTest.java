ee2a07224d7f2666e5ecbe3a1890fcdaa89457ca
Miguel Reboiro-Jato <mrjato@uvigo.es>
2017-04-25 21:07:07 +0200

    Adds the fake and mock tests
    To implementations of the same test have been added. The first one uses a fake
    to replace the remote server (it uses a Jetty as the remote server). The second
    one uses mocks to replace the remote server (with EasyMock and PowerMock).

---
+++ src/test/java/es/uvigo/esei/daa/testdoubles/WebClientFakeTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2015 Miguel Reboiro Jato
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.uvigo.esei.daa.testdoubles;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.uvigo.esei.daa.testdoubles.WebClient;
+
+/**
+ * En este caso, los test se hacen utilizando un servidor real. Concretamente,
+ * se hace uso de Jetty, un servidor completo pero ligero, con una
+ * configuración mínima.
+ * 
+ * Al contrario que al usar mocks, en este caso no es necesario definir el
+ * comportamiento de las dependencias a bajo nivel, pues su funcionalidad es
+ * completa. Por lo tanto, en este caso se trata de una prueba de caja negra.
+ * 
+ * @author Miguel Reboiro Jato
+ * @see http://download.eclipse.org/jetty/stable-9/apidocs/
+ */
+public class WebClientFakeTest {
+	private WebClient client;
+	
+	private String helloWorldURL;
+	private String errorURL;
+	
+	private Server server;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.client = new WebClient();
+		
+		// Configuración del servidor Jetty
+		this.server = new Server(8888);
+
+		final SimpleHandler handler = new SimpleHandler();
+		handler.addMapping("/helloworld", "Hello World");
+		
+		this.server.setHandler(handler);
+		
+		this.server.start();
+		
+		this.helloWorldURL = "http://localhost:8888/helloworld";
+		this.errorURL = "http://localhost:8888/missing";
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.server.stop();
+	}
+
+	@Test
+	public final void testHelloWorld() throws IOException {
+		final URL url = new URL(this.helloWorldURL);
+		
+		assertEquals(
+			"Hello World",
+			this.client.getContent(url)
+		);
+	}
+	
+	@Test(expected = FileNotFoundException.class)
+	public final void testError() throws IOException {
+		final URL url = new URL(this.errorURL);
+		
+		this.client.getContent(url);
+	}
+	
+	private static final class SimpleHandler extends AbstractHandler {
+		private final Map<String, String> mapping;
+		
+		public SimpleHandler() {
+			this.mapping = new HashMap<String, String>();
+		}
+		
+		public void addMapping(String key, String content) {
+			this.mapping.put(key, content);
+		}
+		
+		@Override
+		public void handle(
+			String target,
+			Request baseRequest, 
+			HttpServletRequest request,
+			HttpServletResponse response
+		) throws IOException, ServletException {
+			response.setContentType("text/html;charset=utf-8");
+			baseRequest.setHandled(true);
+			
+			if (this.mapping.containsKey(target)) {
+				response.setStatus(HttpServletResponse.SC_OK);
+
+				final PrintWriter writer = response.getWriter();
+				writer.write(this.mapping.get(target));
+				writer.flush();
+			} else {
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			}
+		}
+	}
+}
---
+++ src/test/java/es/uvigo/esei/daa/testdoubles/WebClientMockTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2015 Miguel Reboiro Jato
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.uvigo.esei.daa.testdoubles;
+
+import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.replay;
+import static org.powermock.api.easymock.PowerMock.verify;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import es.uvigo.esei.daa.testdoubles.WebClient;
+
+/**
+ * En este caso, los tests se hacen usando un mock de URL que nos permite
+ * definir el comportamiento concreto que queremos que exhiba y, además, nos
+ * permite comprobar si el objeto testeado hace las invocaciones esperadas.
+ * 
+ * Por lo tanto, este tipo de prueba se puede considerar de caja negra, pues en
+ * los asserts se evalúa la salida ante una entrada determinada, y de caja
+ * blanca, pues se evalúa el flujo de ejecución a través del objeto "mockeado".
+ * 
+ * @author Miguel Reboiro-Jato
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(WebClient.class)
+public class WebClientMockTest {
+	private WebClient client;
+	
+	private URL mockURL;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.client = new WebClient();
+		
+		// Creación del mock de URL
+		this.mockURL = createMock(URL.class);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		verify(this.mockURL);
+	}
+
+	@Test
+	public final void testHelloWorld() throws IOException {
+		expect(this.mockURL.openStream())
+			.andReturn(new ByteArrayInputStream("Hello World".getBytes()));
+		
+		replay(this.mockURL);
+		
+		assertEquals(
+			"Hello World",
+			this.client.getContent(this.mockURL)
+		);
+	}
+	
+	@Test(expected = FileNotFoundException.class)
+	public final void testError() throws IOException {
+		expect(this.mockURL.openStream())
+			.andThrow(new FileNotFoundException());
+		
+		replay(this.mockURL);
+		
+		this.client.getContent(this.mockURL);
+	}
+}
